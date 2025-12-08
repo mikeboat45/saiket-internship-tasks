8ef65dd7b13c5e6dcf02dd3694cd2fda3f759f7a
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-12-08 14:31:58 +0000

    chore: setup card structure for styling

--- Task-Five-Six/js/main.js
+++ Task-Five-Six/js/main.js
@@ -14,22 +14,26 @@ if (emptyContainer && posts.length === 0) {
   emptyContainer.appendChild(textDisplay);
 } else {
   posts.forEach((post) => {
-    const cardHTML = `<div class="post-card">
+    const cardHTML = `<article class="post-card">
                 <img src="${
                   post.img || "/Task-Five-Six/assets/placeholder.png"
                 }" alt="Post image" class="post-image">
 
-                <div>
+                <div class="card-info"><div class="card-top">
                 <h3>${post.title}</h3>
                 <p class="category">${post.category}</p>
                 </div>
-                <p><strong>${post.author}</strong> — ${post.date}</p>
+                <p><strong>${post.author}</strong> — <span>${
+      post.date
+    }</span></p>
                 
 
                 <p class="excerpt">${post.content.substring(0, 100)}...</p>
 
-                <a href="read.html?id=${post.id}" class="read-btn">Read Post</a>
-            </div>`;
+                <a href="read.html?id=${
+                  post.id
+                }" class="read-btn">Read Post <i class="fa-solid fa-link"></i></a></div>
+            </article>`;
 
     postsContainer.insertAdjacentHTML("afterbegin", cardHTML);
   });
